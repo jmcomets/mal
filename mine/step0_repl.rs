aea4242e7ce0d955c1fec2f7b00f421a6153a5b9
Jean-Marie Comets <jean.marie.comets@gmail.com>
2019-03-03 20:03:28 +0100

    Step 0: complete

---
+++ mine/step0_repl.rs
@@ -0,0 +1,43 @@
+#![deny(warnings)]
+
+use std::io::{
+    self,
+    Write
+};
+
+fn read(s: &str) -> &str {
+    s
+}
+
+fn eval(s: &str) -> &str {
+    s
+}
+
+fn print(s: &str) -> &str {
+    s
+}
+
+fn rep(s: &str) -> &str {
+    print(eval(read(s)))
+}
+
+fn main() -> io::Result<()> {
+    let input = io::stdin();
+    let mut output = io::stdout();
+
+    let mut line = String::new();
+    loop {
+        write!(&mut output, "user> ")?;
+        output.flush()?;
+
+        line.clear();
+        let nb_bytes_read = input.read_line(&mut line)?;
+        if nb_bytes_read == 0 {
+            break;
+        }
+        write!(&mut output, "{}", rep(&line))?;
+        output.flush()?;
+    }
+
+    Ok(())
+}
