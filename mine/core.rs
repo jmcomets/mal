355c13a89639b432bbb01a7d1d023e3ea81dd639
Jean-Marie Comets <jean.marie.comets@gmail.com>
2019-03-19 00:05:34 +0100

    Step 4: add some more operators

--- mine/core.rs
+++ mine/core.rs
@@ -9,5 +9,14 @@ pub(crate) fn ns() -> HashMap<String, MalType> {
     symbols.insert("*".to_string(), binary_operator!(Int * Int -> Int));
     symbols.insert("/".to_string(), binary_operator!(Int / Int -> Int));
 
+    symbols.insert("<".to_string(), binary_operator!(Int < Int -> Bool));
+    symbols.insert(">".to_string(), binary_operator!(Int > Int -> Bool));
+    symbols.insert("<=".to_string(), binary_operator!(Int <= Int -> Bool));
+    symbols.insert(">=".to_string(), binary_operator!(Int >= Int -> Bool));
+
+    // TODO implement for all types
+    symbols.insert("==".to_string(), binary_operator!(Int == Int -> Bool));
+    symbols.insert("!=".to_string(), binary_operator!(Int != Int -> Bool));
+
     symbols
 }
